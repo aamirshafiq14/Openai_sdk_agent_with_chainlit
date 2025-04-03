233e520cf804b925a6c858c9174d97d063fd25af
aamirshafiq14 <aamirshafiq1407@gmail.com>
2025-04-03 23:02:18 +0500

    Making SDK Agent with Chainlit

---
+++ src/project3/hello.py
@@ -0,0 +1,20 @@
+import chainlit as cl
+
+@cl.on_start_message
+async def on_start_message(message: cl.Message):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
